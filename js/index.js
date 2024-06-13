0cf7c4e35e074120f5156be86e2f2e5d0dd36587
Louiexz <fluizlucas@gmail.com>
2024-06-12 22:37:51 -0300

    Tela declaração

--- js/index.js
+++ js/index.js
@@ -2,25 +2,28 @@ import { FuncoesTexto } from "../js/texto.js";
 import { Slide } from "../js/slide.js";
 
 var marquee = new FuncoesTexto();
-var slide = new Slide();
 
 marquee.criaMarquee();
-slide.adicionaEventos();
-slide.atualizarImagem();
 
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slides');
+if (! window.location.href.includes('declaracao.html')) {
+  var slide = new Slide();
+  slide.adicionaEventos();
+  slide.atualizarImagem();
 
-function showSlide() {
-  slides.forEach(slide => {
-    slide.style.display = 'none';
-  });
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].style.display = 'block';
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slides');
+
+  function showSlide() {
+    slides.forEach(slide => {
+      slide.style.display = 'none';
+    });
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = 'block';
+  }
+
+  // Define o intervalo de tempo para mudar os slides (em milissegundos)
+  const intervalo = 2000; // 5 segundos
+
+  // Inicia o intervalo
+  setInterval(showSlide, intervalo);
 }
-
-// Define o intervalo de tempo para mudar os slides (em milissegundos)
-const intervalo = 2000; // 5 segundos
-
-// Inicia o intervalo
-setInterval(showSlide, intervalo);
